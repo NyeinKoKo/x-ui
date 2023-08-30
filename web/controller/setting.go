2371b84a700cf624921acd9293387997e0dcf727
NyeinKoKo <55767055+NyeinKoKo@users.noreply.github.com>
2023-08-31 03:38:33 +0630

    Update setting.go

--- web/controller/setting.go
+++ web/controller/setting.go
@@ -40,49 +40,49 @@ func (a *SettingController) initRouter(g *gin.RouterGroup) {
 func (a *SettingController) getAllSetting(c *gin.Context) {
 	allSetting, err := a.settingService.GetAllSetting()
 	if err != nil {
-		jsonMsg(c, "获取设置", err)
-		return
-	}
-	jsonObj(c, allSetting, nil)
+		jsonMsg(c, "Get settings", err)
+return
+}
+jsonObj(c, allSetting, nil)
 }
 
 func (a *SettingController) updateSetting(c *gin.Context) {
-	allSetting := &entity.AllSetting{}
-	err := c.ShouldBind(allSetting)
-	if err != nil {
-		jsonMsg(c, "修改设置", err)
-		return
-	}
-	err = a.settingService.UpdateAllSetting(allSetting)
-	jsonMsg(c, "修改设置", err)
+allSetting := &entity.AllSetting{}
+err := c.ShouldBind(allSetting)
+if err != nil {
+jsonMsg(c, "Modify settings", err)
+return
+}
+err = a.settingService.UpdateAllSetting(allSetting)
+jsonMsg(c, "Modify settings", err)
 }
 
 func (a *SettingController) updateUser(c *gin.Context) {
-	form := &updateUserForm{}
-	err := c.ShouldBind(form)
-	if err != nil {
-		jsonMsg(c, "修改用户", err)
-		return
-	}
-	user := session.GetLoginUser(c)
-	if user.Username != form.OldUsername || user.Password != form.OldPassword {
-		jsonMsg(c, "修改用户", errors.New("原用户名或原密码错误"))
-		return
-	}
-	if form.NewUsername == "" || form.NewPassword == "" {
-		jsonMsg(c, "修改用户", errors.New("新用户名和新密码不能为空"))
-		return
-	}
-	err = a.userService.UpdateUser(user.Id, form.NewUsername, form.NewPassword)
-	if err == nil {
-		user.Username = form.NewUsername
-		user.Password = form.NewPassword
-		session.SetLoginUser(c, user)
-	}
-	jsonMsg(c, "修改用户", err)
+form := &updateUserForm{}
+err := c.ShouldBind(form)
+if err != nil {
+jsonMsg(c, "Modify user", err)
+return
+}
+user := session.GetLoginUser(c)
+if user.Username != form.OldUsername || user.Password != form.OldPassword {
+jsonMsg(c, "Modify user", errors.New("The original username or password is wrong"))
+return
+}
+if form. NewUsername == "" || form. NewPassword == "" {
+jsonMsg(c, "Modify user", errors.New("The new user name and new password cannot be empty"))
+return
+}
+err = a.userService.UpdateUser(user.Id, form.NewUsername, form.NewPassword)
+if err == nil {
+user.Username = form.NewUsername
+user.Password = form.NewPassword
+session.SetLoginUser(c, user)
+}
+jsonMsg(c, "Modify user", err)
 }
 
 func (a *SettingController) restartPanel(c *gin.Context) {
-	err := a.panelService.RestartPanel(time.Second * 3)
-	jsonMsg(c, "重启面板", err)
+err := a.panelService.RestartPanel(time.Second * 3)
+jsonMsg(c, "Restart Panel", err)
 }
